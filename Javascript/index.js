c8371ca7a31840f1f673217d55edb9a0f1aa9072
Aldot-02 <aldotwizerimana1@gmail.com>
2024-02-12 11:32:22 +0200

    Dashboard's responsiveness

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,11 +1,4 @@
 window.onload = function(){
-    window.addEventListener('scroll', function(e) {
-        if(window.pageYOffset > 100) {
-            document.querySelector("header").classList('is-scrolling');
-        } else {
-            document.querySelector("header").classList.remove('is-scrolling');
-        }
-    });
 
     const menu_hamburger = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
@@ -14,10 +7,6 @@ window.onload = function(){
     menu_hamburger.addEventListener('click', function(){
         menu_hamburger.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
-        mobile_menu_nav.classList.toggle('is-active');
     });
 
-    mobile_menu_nav.addEventListener('click', function(){
-        mobile_menu.classList.toggle('is-active');
-    })
 }
